72a08b734b90c192910bc2ee970d76a691d62cae
Jheymerson Aranha <jheymersonla@gmail.com>
2025-08-22 15:20:08 +0000

    adiciona um container, sabe parecido com aquele do bootstrap que em tela

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -10,7 +10,7 @@ export function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <main className="container mx-auto flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <StatsCards />
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
           <div className="xl:col-span-2">
